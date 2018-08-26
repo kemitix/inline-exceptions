757a11bcccb8ecb06e55a5dc5bfb40489c50b1e7
Paul Campbell <pcampbell@kemitix.net>
2018-08-26 11:17:31 +0100

    User AssertJ in tests

--- src/test/java/net/kemitix/inline/exceptions/InlineExceptionsTest.java
+++ src/test/java/net/kemitix/inline/exceptions/InlineExceptionsTest.java
@@ -1,106 +1,51 @@
 package net.kemitix.inline.exceptions;
 
+import org.assertj.core.api.WithAssertions;
 import org.junit.Test;
 
-/**
- * Tests for {@link InlineExceptions} and {@link InlineExceptions.InlineClause}.
- *
- * @author pcampbell
- */
-public class InlineExceptionsTest {
+public class InlineExceptionsTest implements WithAssertions {
 
-    /**
-     * Test that exception is thrown for
-     * {@link InlineExceptions.InlineClause#should(boolean)} should(true)}.
-     *
-     * @throws InlineException                if there is an error in the test
-     * @throws ArrayIndexOutOfBoundsException is test passes
-     * @throws Exception                      if the test passes (specifically
-     *                                        ArrayIndexOufOfBoundsException)
-     */
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void shouldThrowExceptionWhenShouldIsTrue()
-            throws InlineException, ArrayIndexOutOfBoundsException, Exception {
-        //when
-        InlineExceptions.doThrow(ArrayIndexOutOfBoundsException.class, "pass")
-                .should(true);
-    }
-
-    /**
-     * Test that exception is not thrown for
-     * {@link InlineExceptions.InlineClause#should(boolean) should(false)}.
-     *
-     * @throws InlineException                if there is an error in the test
-     * @throws ArrayIndexOutOfBoundsException is test fails
-     * @throws Exception                      if the test fails (specifically
-     *                                        ArrayIndexOufOfBoundsException)
-     */
     @Test
-    public void shouldNotThrowExceptionWhenShouldIsFalse()
-            throws InlineException, ArrayIndexOutOfBoundsException, Exception {
-        //when
-        InlineExceptions.doThrow(ArrayIndexOutOfBoundsException.class, "pass")
-                .should(false);
-        //then
-        // no exception thrown is a pass
+    public void shouldThrowExceptionWhenShouldIsTrue() {
+        assertThatCode(() ->
+                InlineExceptions.doThrow(ArrayIndexOutOfBoundsException.class, "pass")
+                        .should(true))
+                .isInstanceOf(ArrayIndexOutOfBoundsException.class);
     }
 
-    /**
-     * Test that exception is thrown for
-     * {@link InlineExceptions.InlineClause#unless(boolean) unless(false)}.
-     *
-     * @throws InlineException                if there is an error in the test
-     * @throws ArrayIndexOutOfBoundsException is test passes
-     * @throws Exception                      if the test passes (specifically
-     *                                        ArrayIndexOufOfBoundsException)
-     */
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void shouldThrowExceptionWhenUnlessIsFalse()
-            throws InlineException, ArrayIndexOutOfBoundsException, Exception {
-        //when
-        InlineExceptions.doThrow(ArrayIndexOutOfBoundsException.class, "pass")
-                .unless(false);
-    }
-
-    /**
-     * Test that exception is not thrown for
-     * {@link InlineExceptions.InlineClause#unless(boolean) unless(true)}.
-     *
-     * @throws InlineException                if there is an error in the test
-     * @throws ArrayIndexOutOfBoundsException is test fails
-     * @throws Exception                      if the test fails (specifically
-     *                                        ArrayIndexOufOfBoundsException)
-     */
     @Test
-    public void shouldNotThrowExceptionWhenUnlessIsTrue()
-            throws InlineException, ArrayIndexOutOfBoundsException, Exception {
-        //when
-        InlineExceptions.doThrow(ArrayIndexOutOfBoundsException.class, "pass")
-                .unless(true);
-        //then
-        // no exception thrown is a pass
+    public void shouldNotThrowExceptionWhenShouldIsFalse() {
+        assertThatCode(() ->
+                InlineExceptions.doThrow(ArrayIndexOutOfBoundsException.class, "pass")
+                        .should(false))
+                .doesNotThrowAnyException();
     }
 
-    /**
-     * Test that exception is thrown when trying to instantiate a utility class.
-     */
-    @Test(expected = UnsupportedOperationException.class)
+    @Test
+    public void shouldThrowExceptionWhenUnlessIsFalse() {
+        assertThatCode(() ->
+                InlineExceptions.doThrow(ArrayIndexOutOfBoundsException.class, "pass")
+                        .unless(false))
+                .isInstanceOf(ArrayIndexOutOfBoundsException.class);
+    }
+
+    @Test
+    public void shouldNotThrowExceptionWhenUnlessIsTrue() {
+        assertThatCode(() ->
+                InlineExceptions.doThrow(ArrayIndexOutOfBoundsException.class, "pass")
+                        .unless(true))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
     public void shouldThrowExceptionOnUtilityInstantiation() {
-        //when
-        InlineExceptions failed = new InlineExceptions();
-        //then
-        // failed if we didn't throw an exception
+        assertThatCode(InlineExceptions::new)
+                .isInstanceOf(UnsupportedOperationException.class);
     }
 
-    /**
-     * Test that attempting to create an invalid clause will throw an
-     * {@link InlineException}.
-     *
-     * @throws InlineException if test passes
-     */
-    @Test(expected = InlineException.class)
-    public void shouldThrowInlineException() throws InlineException {
-        //when
-        InlineExceptions.doThrow(null, null);
+    @Test
+    public void shouldThrowInlineException() {
+        assertThatCode(() -> InlineExceptions.doThrow(null, null))
+                .isInstanceOf(InlineException.class);
     }
 }
